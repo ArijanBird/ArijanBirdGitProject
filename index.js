db582bd65f7d9c44825c32026923555f1478d357
ArijanBird <a78906142@gmail.com>
2026-06-25 18:44:32 -0700

    change-color

--- index.js
+++ index.js
@@ -1,14 +1,2 @@
-function changeText() {
-  const title = document.getElementById('title');
-
-  if (title.innerText === 'Hello World') {
-    title.innerText = 'You clicked the button!';
-    title.style.color = 'green';
-  } else {
-    title.innerText = 'Hello World';
-    title.style.color = 'black';
-  }
-}
-
-// Optional: log page loaded
-console.log('App loaded successfully');
+const div = document.getElementById('test');
+div.style.color = 'red';
